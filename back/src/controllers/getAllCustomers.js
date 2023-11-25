9ad89e00311a394e224cd4da9eddfb4b822728f0
MoniMcI <mbguantay@yahoo.com.ar>
2023-11-25 19:53:53 -0300

    commit4

--- back/src/controllers/getAllCustomers.js
+++ back/src/controllers/getAllCustomers.js
@@ -7,9 +7,10 @@ const OPENPAY_PRIVATE_KEY = process.env.OPENPAY_PRIVATE_KEY;
 
 const openpay = new Openpay(OPENPAY_MERCHANT_ID, OPENPAY_PRIVATE_KEY, false);
 
-
-const getAllCustomers = (searchParams) => {
-    return new Promise((resolve, reject) => {
+const getAllCustomers = async (searchParams) => {
+  try {
+    // Obtener clientes de OpenPay
+    const openPayCustomers = await new Promise((resolve, reject) => {
       openpay.customers.list(searchParams, (error, customers) => {
         if (error) {
           reject(error);
@@ -18,6 +19,34 @@ const getAllCustomers = (searchParams) => {
         }
       });
     });
-  };
+
+    //agregar a bd
+    for (const openPayCustomer of openPayCustomers) {
+      const existingCustomer = await Cliente.findOne({
+        where: { id: openPayCustomer.id }
+      });
+
+      if (!existingCustomer) {
+        await Cliente.create({
+          id: openPayCustomer.id,
+          name: openPayCustomer.name,
+          last_name: openPayCustomer.last_name,
+          email: openPayCustomer.email,
+          phone_number: openPayCustomer.phone_number,
+          city: openPayCustomer.address ? openPayCustomer.address.city : null,
+          state: openPayCustomer.address ? openPayCustomer.address.state : null,
+          line1: openPayCustomer.address ? openPayCustomer.address.line1 : null,
+          postal_code: openPayCustomer.address ? openPayCustomer.address.postal_code : null,
+          country_code: openPayCustomer.address ? openPayCustomer.address.country_code : null,
+        });
+      }
+    }
+
+    return openPayCustomers;
+  } catch (error) {
+    console.error('Error al obtener y sincronizar clientes:', error);
+    throw error;
+  }
+};
 
 module.exports = getAllCustomers;
